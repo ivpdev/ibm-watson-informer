8483cfe73d6804e28cee2d6bbe63774dbd49cbf5
ivp <ivp@example.com>
2017-07-19 16:55:26 +0200

    added UI

---
+++ services/WatsonConversationService.js
@@ -0,0 +1,16 @@
+
+
+var WatsonConversationService = {
+	sendMessage: function(msg) {
+		console.log('m: ' + msg);
+		return new Promise((resolve, reject) => {
+
+				resolve('STUB');
+
+                       })
+
+
+	}
+}
+
+module.exports = WatsonConversationService;
