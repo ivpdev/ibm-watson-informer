9619f6e12520387e5776cdcab39e89c211ca5f27
ivp <ivp@example.com>
2017-08-03 11:50:41 +0200

    debuggin bluemix

--- rest/status.js
+++ rest/status.js
@@ -1,7 +1,12 @@
 var express = require('express')
-var router = express.Router();
+var router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.send('OK!') });
+    res.send('OK!') })
+
+
+router.get('/debug', function(req, res, next) {
+    res.send(JSON.stringify(process.env.VCAP_SERVICES, null, 4))
+})
 
 module.exports = router;
